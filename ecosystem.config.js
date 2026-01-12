e0c720393304e8631557e7aed7dc0e9b9fdf3469
markerxz <markerxz@gmail.com>
2026-01-13 02:32:05 +0700

    Fix ecosystem.config.js cwd path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps: [{
     name: 'f1-telemetry',
-    script: 'server.js',
+    script: './server.js',
+    cwd: '/home/opc/f1-telemetry-poc',
     env: {
       LD_LIBRARY_PATH: '/usr/lib/oracle/19.29/client64/lib',
       TNS_ADMIN: '/home/opc/.oracle/wallets/f1_adb_oci',
